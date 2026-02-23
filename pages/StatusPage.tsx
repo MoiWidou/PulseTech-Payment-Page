ba0a80fff73b3ad5e166238a3817acaf7ff29a16
Moises Blanca <moisesblanca@fourptzero.com>
2026-02-23 13:43:31 +0800

    Testing pending to be success

--- pages/StatusPage.tsx
+++ pages/StatusPage.tsx
@@ -2,7 +2,7 @@ import { useSearchParams, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SuccessModal from "../components/modals/SuccessModal";
 import FailedModal from "../components/modals/FailedModal";
-import PendingModal from "../components/modals/PendingModal";
+// import PendingModal from "../components/modals/PendingModal";
 
 type redirectResponse = {
     transaction_id: string,
